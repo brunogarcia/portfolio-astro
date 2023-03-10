ba0b8dfae617ce712b76095254d34e2970e5d049
Bruno Garcia <bruno.garcia.echegaray@gmail.com>
2023-03-10 20:24:24 +0100

    feat: update home

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,10 +4,6 @@ export const MENU_LINKS = [
 		path: "/",
 	},
 	{
-		title: "About",
-		path: "/about",
-	},
-	{
 		title: "Blog",
 		path: "/posts",
 	},
@@ -15,7 +11,13 @@ export const MENU_LINKS = [
 
 // ! Remember to add your own socials
 export const SOCIAL_LINKS = {
-	github: "https://github.com/chrismwilliams/astro-cactus",
+	github: "https://github.com/brunogarcia",
+	linkedin: "https://www.linkedin.com/in/bruno-garcia-echegaray",
+	strava: "https://www.strava.com/athletes/43126016",
+	goodreads:
+		"http://www.goodreads.com/review/list/4722653-bruno-garcia?page=1&sort=rating&view=covers",
+	filmaffinity: "http://www.filmaffinity.com/es/userratings.php?user_id=738783&orderby=0",
+	spotify: "https://play.spotify.com/user/brunogarcia/playlist/4MRGjKqlWuJZJ8XHOGcqkR",
 	twitter: "https://github.com/chrismwilliams/astro-cactus",
 	discord: "https://github.com/chrismwilliams/astro-cactus",
 	email: "me@example.com",
