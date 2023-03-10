dec26f7386842bfe1ca6dc7b5902d10bdcaa563f
Bruno Garcia <bruno.garcia.echegaray@gmail.com>
2023-03-10 20:25:18 +0100

    feat: update home

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -9,7 +9,6 @@ export const MENU_LINKS = [
 	},
 ];
 
-// ! Remember to add your own socials
 export const SOCIAL_LINKS = {
 	github: "https://github.com/brunogarcia",
 	linkedin: "https://www.linkedin.com/in/bruno-garcia-echegaray",
@@ -18,7 +17,4 @@ export const SOCIAL_LINKS = {
 		"http://www.goodreads.com/review/list/4722653-bruno-garcia?page=1&sort=rating&view=covers",
 	filmaffinity: "http://www.filmaffinity.com/es/userratings.php?user_id=738783&orderby=0",
 	spotify: "https://play.spotify.com/user/brunogarcia/playlist/4MRGjKqlWuJZJ8XHOGcqkR",
-	twitter: "https://github.com/chrismwilliams/astro-cactus",
-	discord: "https://github.com/chrismwilliams/astro-cactus",
-	email: "me@example.com",
 };
